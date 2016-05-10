289d16e5efdfb976e2573439a3fa032354d93e00
VanqIvanova <v_ivanova.kn@abv.bg>
2016-05-11 00:09:49 +0300

    commit

--- src/main/java/com/lirex/controllers/HomeController.java
+++ src/main/java/com/lirex/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package com.lirex.controllers;
 
+import com.lirex.model.User;
+import com.lirex.repository.UserRepository;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by vivanova on 4/28/2016.
  */
+@Log
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String homePage() {
@@ -19,6 +31,20 @@ public class HomeController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "login";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String index(@Valid @ModelAttribute(value="user") User user, BindingResult result, Model model) {
+        model.addAttribute("user", user);
+
+
+        if (result.hasErrors()) {
+        log.info("alabalaportokala");
+            return "redirect:/login";
+        }
+        userRepository.save(user);
+
+        return "redirect:/login";
     }
 
 
--- src/main/java/com/lirex/security/ApplicationSecurity.java
+++ src/main/java/com/lirex/security/ApplicationSecurity.java
@@ -2,10 +2,13 @@ package com.lirex.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 /**
  * Created by vivanova on 4/28/2016.
@@ -14,6 +17,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+/*    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(new ShaPasswordEncoder(256))
+                .usersByUsernameQuery(
+                        "SELECT email AS username, password, TRUE FROM user WHERE email = ? AND status = 'ACTIVE'")
+                .authoritiesByUsernameQuery(
+                        "SELECT email AS username, role AS authorities FROM user WHERE email = ? AND status = 'ACTIVE'");
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
